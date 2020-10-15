7fcfa6eca4c5f029feb3910876539d20bd97d415
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2020-10-14 21:25:14 -0400

    All MVPs are done

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, {useReducer, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TodoList } from './Components/TodoList';
 
 function App() {
+
+  // const [todoItem, dispatch] = useReducer(reducerfcn, []);
+  // const [todoList, setTodoList] = useState("");
+
+  // const changeHandler = (e) => { 
+  //   setTodoList(e.target.value);
+  // } 
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   dispatch({type: ACTIONS.ADDTODO});
+  //   setTodoList('');
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo App</h1>
+      <TodoList />
+      {/* <form onSubmit = {handleSubmit}>
+        <input type="text" onChange={changeHandler} value={todoList}/>
+        <button>Completed</button>
+        <button>Clear Todo</button>
+      </form> */}
     </div>
-  );
+  )
 }
 
 export default App;
